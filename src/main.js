c7ab66491af847e7cc9a682d079bfbe1b55ddfa0
vick On Win10 puro <vickfaby1@gmail.com>
2022-08-05 16:12:10 -0500

    trending preview

--- src/main.js
+++ src/main.js
@@ -1,2 +1,29 @@
+async function getTrendingMoviesPreview(){
+    const res = await fetch(
+        'http://api.themoviedb.org/3/trending/movie/day?api_key='
+        + API_KEY);
+    const data = await res.json();
 
-console.log("exitoso!!");
+    const movies = data.results;
+    console.log({data, movies});
+
+    movies.forEach(movie => {
+        const trendingPreviewMoviesContainer = document.querySelector
+        ('#trendingPreview .trendingPreview-movieList');
+        const movieContainer = document.createElement('div');
+        movieContainer.classList.add('movie-container');
+
+        const movieImg = document.createElement('img');
+        movieImg.classList.add('movie-img');
+        movieImg.setAttribute('alt', movie.title);
+        movieImg.setAttribute(
+            'src',
+            'https://image.tmdb.org/t/p/w300' + movie.poster_path
+        );
+        
+        movieContainer.appendChild(movieImg);
+        trendingPreviewMoviesContainer.appendChild(movieContainer);
+    });
+}
+
+getTrendingMoviesPreview();
